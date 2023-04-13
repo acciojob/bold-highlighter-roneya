210338decf9f6be9d83ecf194d0337895b724cd2
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-04-13 22:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+var strong = document.querySelectorAll("strong");
+      for (var i = 0; i < strong.length; i++) {
+        strong[i].style.color = "green";
+      }
 
 }
 
 
 function return_normal() {
-    //Write your code here
+     var strong = document.querySelectorAll("strong");
+      for (var i = 0; i < strong.length; i++) {
+        strong[i].style.color = "black";
+      }
 
     
 }
